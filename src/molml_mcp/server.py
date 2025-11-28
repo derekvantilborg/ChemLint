1f709b9b3442820b4bbbf4a51da4300fc7c416c4
derekvantilborg <dwvantilborg@gmail.com>
2025-11-28 13:49:13 +0100

    improved tool adding

--- src/molml_mcp/server.py
+++ src/molml_mcp/server.py
@@ -1,17 +1,26 @@
 # from .tools import get_all_tools
 
+# src/molml_mcp/server.py
+
 
 # server.py
 from mcp.server.fastmcp import FastMCP
 from molml_mcp.tools import canonicalize_smiles
 
-from molml_mcp.resources.logistics import load_csv
+# All tools we want to expose via the MCP server
+from molml_mcp.resources.logistics import get_all_resource_functions
+
 
 
 # create an MCP server 
 mcp = FastMCP("molml-mcp") 
 
+# Add resource management tools
+for tool_func in get_all_resource_functions():
+    mcp.add_tool(tool_func)
+
+
 # Add tools
 mcp.add_tool(canonicalize_smiles)
-mcp.add_tool(load_csv)
+
 
